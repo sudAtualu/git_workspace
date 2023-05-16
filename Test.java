04576b2b1baa5861ed0ff6c4881b6eb8debc73c6
nhw12299 <nhw12299@naver.com>
2023-05-16 15:21:15 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,11 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("안녕하세요 저는 자바입니다.");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + "번 째 출력");
+            
+        }
     }
     
 }
